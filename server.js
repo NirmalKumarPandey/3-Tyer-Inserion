978769719de75ae9f14f634c90fcbdb4baff9751
Nirmal Kumar <37977837+NirmalKumarPandey@users.noreply.github.com>
2024-06-01 14:56:11 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,111 @@
+let mongoose = require("mongoose");
+let express = require("express");
+let cors = require("cors");
+
+let app = express();
+app.use(cors());
+
+app.get("/players", async (req, res) => {
+    let storedData = await Player.find();
+    res.json(storedData);
+})
+
+
+app.listen(4567, () => {
+    console.log("Listening to port 4567");
+})
+
+
+let playerSchema = new mongoose.Schema({
+    playerName: {
+        type: String,
+        validate: {
+            validator: function (v) {
+                return /^[a-zA-Z]+([ '-][a-zA-Z]+)*$/.test(v);
+            },
+            message: props => `${props.value} is not a valid Name!`
+        },
+        required: [true, 'User Name is required'],
+    },
+    age: {
+        type: Number,
+        min: [1, "Invalid Values "],
+        max: [70, "Invalid Values"],
+        required: true,
+    },
+    teamName: {
+        type: String,
+        enum: ["csk", "mi", "srh", "kkr", "rcb", "delhi", "rr", "Isg", "gt"],
+        lowercase: true,
+    },
+    email: {
+        type: String,
+        validate: {
+            validator: function (v) {
+                return /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(v);
+            },
+            message: props => `${props.value} is not a valid email!`
+        },
+        required: [true, 'User email is required'],
+    },
+    nationality: String,
+
+});
+let Player = new mongoose.model("players", playerSchema);
+
+let getDataFromDatabase = async () => {
+    let dataFromDatabase = await Player.find();
+    console.log(dataFromDatabase);
+}
+
+
+let saveDataIntoDatabase = async () => {
+
+    try {
+        let shreyasIyer = new Player({
+            playerName: "ShreyasIyer Kumar",
+            age: 36,
+            teamName: "DELHI",
+            email: "shreyasiyer@gmail.com",
+            nationality: "Indian",
+
+        });
+        let nirmalKumar = new Player({
+            playerName: "Nirmal Kumar",
+            age: 46,
+            teamName: "RCB",
+            email: "nirmalnirman36@gmail.com",
+            nationality: "Indian",
+
+        });
+        let bholuKumar = new Player({
+            playerName: "Bholu Kumar",
+            age: 46,
+            teamName: "kkr",
+            email: "bholu@gmail.com",
+            nationality: "Indian",
+        });
+        Player.insertMany([shreyasIyer, bholuKumar, nirmalKumar]);
+        console.log("Successfully save data into Mongo Database");
+    }
+    catch (err) {
+        console.log("unable to Store " + err);
+        console.log("Unable to store data into Database" + err);
+    }
+}
+
+let connectToMongoDB = async () => {
+
+    try {
+        await mongoose.connect("mongodb+srv://nirmalirish1234:Nirman%4024@nirmal.p9uda40.mongodb.net/cricket?retryWrites=true&w=majority&appName=Nirmal");
+        saveDataIntoDatabase();
+        // getDataFromDatabase();
+        console.log("You are successfully connected with Mongo DB");
+    }
+    catch (err) {
+        console.log("You are not able to connected with Mongo DB" + err);
+
+    }
+
+}
+connectToMongoDB();
---
+++ src/App.js
@@ -0,0 +1,34 @@
+import './App.css';
+import React, { useState } from 'react';
+
+function App() {
+  let [playerdata, setPlayerdata] = useState([]);
+
+  let getDataFromServer = async () => {
+    let reqOptions = {
+      method: "GET"
+    }
+    let JSONData = await fetch("http://localhost:4567/players", reqOptions);
+    let JSOData = await JSONData.json();
+    setPlayerdata(JSOData);
+    console.log(JSOData);
+  }
+  return (
+    <div className="App">
+      <button type="button" onClick={() => {
+        getDataFromServer();
+      }}>Get Data</button>
+      {playerdata.map((ele, i) => {
+        return <div>
+          <button type='button' id='player'>{ele.playerName}</button>
+          <button type='button' id='age'>{ele.age}</button>
+          <button type='button' id='teamName'>{ele.teamName}</button>
+          <button type='button' id='email'>{ele.email}</button>
+        </div>
+
+      })}
+    </div>
+  );
+}
+
+export default App;
